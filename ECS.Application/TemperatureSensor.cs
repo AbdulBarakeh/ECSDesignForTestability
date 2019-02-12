96423453db6fc5597f9395f74c5c552bd966ee06
Mads Steiner Kristensen <steiner.au@gmail.com>
2019-02-12 16:40:48 +0100

    Implemented new application design with Triple I principle

---
+++ ECS.Application/TemperatureSensor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Application
+{
+    public class TemperatureSensor : ITemperatureSensor
+    {
+        private readonly Random _generator = new Random(); 
+
+        public int GetTemperature()
+        {
+            return _generator.Next(-5, 45);
+        }
+    }
+}
