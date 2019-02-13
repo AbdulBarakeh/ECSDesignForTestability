a97be21f3f6937e31806ba34310c7130e6ff1b18
Mads Steiner Kristensen <steiner.au@gmail.com>
2019-02-13 11:41:52 +0100

    Added unit test file

---
+++ ECS.Test.Unit/ECSUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using ECS.Application;
+
+
+namespace ECS.Test.Unit
+{
+
+    [TestFixture]
+    [Author("Mads Steiner Kristensen")]
+    public class ECSUnitTest
+    {
+        private Application.ECS _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Application.ECS(20, new StubHeater(), new StubTemperatureSensor());
+        }
+
+
+
+
+    }
+}
