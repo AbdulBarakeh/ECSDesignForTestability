6342220d4a0fe0bcce5dbb1cf4ac4967aeac56d4
Mads Steiner Kristensen <steiner.au@gmail.com>
2019-02-13 10:53:53 +0100

    Added unit test project

---
+++ ECS.Test.Unit/FakeTemperatureSensor.cs
@@ -0,0 +1,19 @@
+﻿using ECS.Application;
+
+namespace ECS.Test.Unit
+{
+    public class FakeTemperatureSensor : ITemperatureSensor
+    {
+        public int Temperature { get; set; }
+
+        public FakeTemperatureSensor()
+        {
+            Temperature = 0;
+        }
+
+        public int GetTemperature()
+        {
+            return Temperature;
+        }
+    }
+}
